9b82d498d2ed78da6a5eb5bfb11b49a7c265e5fb
TrueMerc <true_merc@mail.ru>
2018-11-24 00:37:47 +0300

    Add '.gitignore' file, project files and space background picture.

---
+++ star-shooter/desktop/src/ru/ryabtsev/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.ryabtsev.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.ryabtsev.game.StarShooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarShooter(), config);
+	}
+}
