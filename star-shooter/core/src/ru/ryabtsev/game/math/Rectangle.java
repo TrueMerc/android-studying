5c31533806f193b0cad09038b7145415aa72ff0a
TrueMerc <true_merc@mail.ru>
2018-12-01 11:28:37 +0300

    Add 'Rectangle' class.

---
+++ star-shooter/core/src/ru/ryabtsev/game/math/Rectangle.java
@@ -0,0 +1,11 @@
+package ru.ryabtsev.game.math;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Base class for different rectangle regions.
+ */
+public class Rectangle {
+
+    private Vector2 position;
+}
